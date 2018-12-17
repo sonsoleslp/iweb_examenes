7952d3c899e20b6ab004edc570c54cc400350bb4
Sonsoles López Pernas <sonsoleslp@hotmail.com>
2018-12-17 19:06:01 +0100

    Clean

--- ene_18/src/TodoList.jsx
+++ ene_18/src/TodoList.jsx
@@ -1,17 +1,1 @@
-import React from 'react';
-import TodoListElement from './TodoListElement.jsx';
-export default class TodoList extends React.Component {
-	render(){
-		let todos = this.props.todos.map((todo, index)=>{
-			return <TodoListElement
-			key={index}
-			index={index}
-			text={todo.text}
-			completed={todo.completed}
-			todoToggle={this.props.todoToggle} />
-		});
-		return (
-			<ul>{todos}</ul>
-			);
-	}
-}
+ 
